9f3b5f8f2d8880e3be99eaf00d6037f59bd17375
piro <piro.haniwa@gmail.com>
2022-02-11 03:04:31 +0900

    ci: add sitemap and pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
+  reactStrictMode: true,
+});
+
+module.exports = nextConfig;
