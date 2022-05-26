a9b6099944fa3cab8f6f25edd9ce72b4d663d68b
piro <piro.haniwa@gmail.com>
2022-05-27 01:14:45 +0900

    feat: create document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,41 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=M+PLUS+2&family=Zen+Kaku+Gothic+Antique&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@700&display=swap&text=りくばん！"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
